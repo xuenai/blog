3ec5f3eca4fb859ecc5bd799d202c2036e18dcd7
justpeth <justpeth@163.com>
2019-09-11 21:00:51 +0800

    新增login

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import client from '@graphql';
 import clsx from 'clsx';
 import { useStore } from '@config';
 import './App.css';
 
-import {Menu, Search, NotFound} from '@components'
+import { Menu, NotFound } from '@components';
+import Login from '@pages/login/login'
 
 function App() {
-  const {isOpen} = useStore('menu');
+  const { isOpen } = useStore('menu');
   let mianCls = clsx({
     main: true,
     'main--disabled': isOpen
@@ -20,8 +21,10 @@ function App() {
         <>
           <Menu />
           <div className={mianCls}>
-            <Search></Search>
-            <Route component={NotFound}></Route>
+            <Switch>
+              <Route path="/login" component={Login}></Route>
+              <Route component={NotFound}></Route>
+            </Switch>
           </div>
         </>
       </Router>
---
+++ client/src/pages/login/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './login.module.scss'
+
+const Login = () => {
+  document.title = '啦啦啦~~~'
+  return (
+    <div className={styles['login-box']}>
+      <div className={styles.login}>
+        <h2>welcome!</h2>
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Login;
