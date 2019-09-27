6688bd5ad165f7272254eff1ffae9b69586f0879
justpeth <justpeth@163.com>
2019-09-27 12:04:07 +0800

    修改

--- server/src/graphql/resolvers/Query.js
+++ server/src/graphql/resolvers/Query.js
@@ -1,16 +1,15 @@
 import {ApolloError} from 'apollo-server-koa'
 import { getUser, isLogin } from '../../config/utils'
-// import { ValidationError } from '../../config/formatError'
 
 async function users(root, args, { ctx, User }) {
   const user = await isLogin(ctx)
-  const isAdmin = user.isAdmin
-  if (!isAdmin) {
-    // throw new ValidationError({
-    //   key: 'createUser',
-    //   message: '你不是管理员,无法创建新用户,请使用注册通道'
-    // })
-  }
+  // const isAdmin = user.isAdmin
+  // if (!isAdmin) {
+  //   throw new ValidationError({
+  //     key: 'createUser',
+  //     message: '你不是管理员,无法创建新用户,请使用注册通道'
+  //   })
+  // }
 
   return User.find()
 }
